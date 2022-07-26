8d2de3c4dc8be528c0a082cbb6727ed9bea82ee6
jdwilkin4 <jwilkin4@hotmail.com>
2022-07-25 22:10:37 -0700

    chore: setting up backend files

---
+++ script.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const contentful = require("contentful");
+const express = require("express");
+const port = process.env.PORT || 8000;
+const app = express();
+const cors = require("cors");
+let mediaArr = [];
+const client = contentful.createClient({
+  space: process.env.SPACE_ID,
+  accessToken: process.env.ACCESS_TOKEN,
+});
+
+client.getEntries({ content_type: "media" }).then((entries) => {
+  entries.items.forEach((entry) => {
+    mediaArr.push(entry.fields);
+  });
+});
+
+app.get("/api/media", (req, res) => {
+  res.json(mediaArr);
+});
+
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Server has started at port ${port}`);
+});
