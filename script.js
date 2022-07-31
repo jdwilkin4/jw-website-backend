2be5afeadf0e1013fd7c34dde273a9a0ae10c772
jdwilkin4 <jwilkin4@hotmail.com>
2022-07-30 23:31:54 -0700

    feat: adding posts api

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 8000;
 const app = express();
 const cors = require("cors");
 let mediaArr = [];
+let blogPostsArr = [];
+
 const client = contentful.createClient({
   space: process.env.SPACE_ID,
   accessToken: process.env.ACCESS_TOKEN,
@@ -16,10 +18,20 @@ client.getEntries({ content_type: "media" }).then((entries) => {
   });
 });
 
+client.getEntries({ content_type: "blogPosts" }).then((entries) => {
+  entries.items.forEach((entry) => {
+    blogPostsArr.push(entry.fields);
+  });
+});
+
 app.use(cors());
 
 app.get("/api/media", (req, res) => {
   res.json(mediaArr);
+});
+
+app.get("/api/posts", (req, res) => {
+  res.json(blogPostsArr);
 });
 
 app.listen(port, () => {
